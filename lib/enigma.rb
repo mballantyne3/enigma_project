9088476c03963e03e575e83c6b2679ed965748f7
Mary Ballantyne <mballantyne777@gmail.com>
2022-08-06 19:27:41 -0600

    create encrypt method with shifts

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,11 +1,28 @@
 class Enigma
 
-  # def encrypt(message, key, date)
-    # encrypted_code = Hash.new
-    # encrypted_code[message] = the encrypted String
-    # encrypted_code[key] = the key used for encryption as a String
-    # encrypted_code[date] = the date used for encryption as a String DDMMYY
-  # end
+  def encrypt(message, key = nil, date = nil)
+    key ||= KeyGenerator.generate
+    date ||= Date.today.strftime("%d%m%y")
+
+    ka, kb, kc, kd = key[..1], key[1..2], key[2..3], key[3..4]
+    oa, ob, oc, od = OffsetGenerator.generate(date)
+    shifts = [
+      ka.to_i + oa.to_i,
+      kb.to_i + ob.to_i,
+      kc.to_i + oc.to_i,
+      kd.to_i + od.to_i
+    ]
+
+    character_set = ("a".."z").to_a << " "
+
+    encrypted_message = message.chars.map.with_index do |letter, i|
+      letter_position = character_set.index(letter)
+      shifted_position = letter_position + shifts[i % shifts.length]
+      character_set[shifted_position % character_set.length]
+    end.join
+
+    { encryption: encrypted_message, key: key, date: date }
+  end
 
   # def decrypt(decryption, key, date)
   #   decrypted_code = Hash.new
