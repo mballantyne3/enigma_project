e15a05111546a17fc6f4114581199b93ac8c40ca
Mary Ballantyne <mballantyne777@gmail.com>
2022-08-09 15:08:23 -0600

    add additional test to shift generator module

--- spec/shift_generator_spec.rb
+++ spec/shift_generator_spec.rb
@@ -1,9 +1,20 @@
 require './lib/shift_generator'
 
 RSpec.describe ShiftGenerator do
-  it 'generates an array' do
-    shift = ShiftGenerator.generate
+  it 'generates a shift and returns the key and date' do
+    shift, key, date = ShiftGenerator.generate("02715", "090892")
 
-    expect(shift).to be_a(Array)
+    expect(shift).to eq([7, 33, 77, 19])
+    expect(key).to eq("02715")
+    expect(date).to eq("090892")
+  end
+
+  it 'generates keys and dates if you dont pass them' do
+    allow(Date).to receive(:today).and_return(Date.parse('1992-08-09'))
+    allow(KeyGenerator).to receive(:generate).and_return('02715')
+
+    shift, key, date = ShiftGenerator.generate
+    expect(date).to eq('090892')
+    expect(key).to eq('02715')
   end
 end
